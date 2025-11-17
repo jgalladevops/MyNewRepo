f11abd5192be17fb23f2e36f361db1b2cffdd059
root <root@ip-172-31-79-122.ec2.internal>
2025-11-17 14:02:30 +0000

    now i am adding data

--- app.java
+++ app.java
@@ -3,6 +3,7 @@ public class Main {
        System.out.println("Hello World");
    }
 }
+
 //dev2 push from local
 public class AddTwoIntegers {
    public static void main(String[] args) {
@@ -12,3 +13,6 @@ public class AddTwoIntegers {
        System.out.println("The sum is: " + sum);
    }
 }
+=======
+
+dev2 code is added
